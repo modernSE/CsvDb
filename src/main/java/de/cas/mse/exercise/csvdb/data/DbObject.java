08f2ebd11ef4b26396cd58ba5fbaa624aa8ad0b2
modernSE Trainee 1 <keydon+mse-trainee@gmail.com>
2018-02-20 14:29:11 +0100

    Ergebnis der Gruppe 1

--- src/main/java/de/cas/mse/exercise/csvdb/data/DbObject.java
+++ src/main/java/de/cas/mse/exercise/csvdb/data/DbObject.java
@@ -1,14 +1,16 @@
 package de.cas.mse.exercise.csvdb.data;
 
 public interface DbObject {
-	
+	// csv Separator Konstante gehört in die CsvDB Klasse
 	final String CSV_SEPARATOR = ",";
 
 	public String getGuid();
 	
 	public void setGuid(String guid);
 	
+	// csv spezifische Methode gehört zu CsvDB Klasse. Erstellen von DbObject 
+	// gehört nicht zu Interface
 	public DbObject convertFromCsv(String csvLine, String csvSeparator);
-	
+	// Kein Bezug auf csv bei Methoden- und Parameterbenamung
 	public String toCsvLine(String csvSeparator);
 }
