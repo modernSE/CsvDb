dcf18747a74e855ae070baec09d2d934af645166
modernSE Trainee 4 <keydon+mse-trainee@gmail.com>
2018-10-02 16:51:58 +0200

    Ergebnis der Gruppe 4

--- src/main/java/de/cas/mse/exercise/csvdb/impl/AddressDb.java
+++ src/main/java/de/cas/mse/exercise/csvdb/impl/AddressDb.java
@@ -1,14 +1,17 @@
 package de.cas.mse.exercise.csvdb.impl;
 
+import static java.util.stream.Collectors.toList;
+
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import de.cas.mse.exercise.csvdb.CsvDB;
 import de.cas.mse.exercise.csvdb.data.Address;
@@ -16,14 +19,27 @@ import de.cas.mse.exercise.csvdb.data.Address;
 public class AddressDb implements CsvDB<Address> {
 
 	private static final String CSV_SEPARATOR = ",";
-	protected final Path basePath = Paths.get("data").toAbsolutePath();
+	protected final Path basePath;
+
+	public AddressDb() {
+		Path relativePath = Paths.get("data");
+		basePath = relativePath.toAbsolutePath();
+	}
 
 	@Override
 	public Address loadObject(final String guid, final Class<Address> type) {
 		final Path tableFile = determineTableFile();
 		try {
 			final List<String> lines = Files.readAllLines(tableFile);
-			final Optional<String> matchedAddress = lines.stream().filter(e -> e.startsWith(guid)).findAny();
+			Optional<String> matchedAddress = Optional.empty();
+
+			for (String line : lines) {
+				if (line.startsWith(guid)) {
+					matchedAddress = Optional.of(line);
+					break;
+				}
+			}
+
 			return turnToAddress(
 					matchedAddress.orElseThrow(() -> new RuntimeException("address with guid " + guid + "not found")));
 		} catch (final IOException e) {
@@ -47,7 +63,9 @@ public class AddressDb implements CsvDB<Address> {
 		final Path tableFile = determineTableFile();
 		try {
 			final List<String> lines = Files.readAllLines(tableFile);
-			return lines.stream().map(e -> turnToAddress(e)).collect(Collectors.toList());
+			Stream<String> stream = lines.stream();
+			Stream<Address> addressStream = stream.map(this::turnToAddress);
+			return addressStream.collect(toList());
 		} catch (final IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -58,7 +76,8 @@ public class AddressDb implements CsvDB<Address> {
 		setGuidIfNeeded(address);
 		final Path tableFile = determineTableFile();
 		try (final RandomAccessFile file = new RandomAccessFile(tableFile.toFile(), "rw")) {
-			file.seek(file.length());
+			long fileLength = file.length();
+			file.seek(fileLength);
 			file.writeBytes(toCsvLine(address));
 		} catch (final IOException e) {
 			e.printStackTrace();
@@ -73,16 +92,26 @@ public class AddressDb implements CsvDB<Address> {
 	}
 
 	protected String toCsvLine(final Address address) {
-		return address.getGuid() + CSV_SEPARATOR + address.getName() + CSV_SEPARATOR + address.getStreet()
-				+ CSV_SEPARATOR + address.getZip() + CSV_SEPARATOR + address.getTown();
+		String guid = address.getGuid();
+		String name = address.getName();
+		String street = address.getStreet();
+		String zip = address.getZip();
+		String town = address.getTown();
+		List<String> fields = Arrays.asList(guid, name, street, zip, town);
+		return String.join(CSV_SEPARATOR, fields);
 	}
 
 	protected Path determineTableFile() {
-		return basePath.resolve("Address.csv");
+		final String fileName = "Address.csv";
+		return basePath.resolve(fileName);
 	}
 
 	private String createGuid() {
-		return UUID.randomUUID().toString().replaceAll("-", "").toUpperCase();
+		UUID newGuid = UUID.randomUUID();
+		String newGuidAsString = newGuid.toString();
+		String partiallyNormalizedGuid = newGuidAsString.replaceAll("-", "");
+
+		return partiallyNormalizedGuid.toUpperCase();
 	}
 
 }
